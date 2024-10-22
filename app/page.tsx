f0af6c62a763261e77c68c0ed3534d25a7bba823
Dmitry Selikhov <selikhov.dmitrey@gmail.com>
2024-10-22 02:16:38 -0400

    -

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,12 @@ export const metadata: Metadata = {
     card: "summary_large_image",
   },
   openGraph: {
-    url: "https://next-jobs.vercel.app/",
+    url: "https://elevanceit.vercel.app/",
     images: [
       {
         width: 1200,
         height: 630,
-        url: "https://raw.githubusercontent.com/Blazity/next-jobs/main/.github/assets/project-logo.png",
+        url: "https://raw.githubusercontent.com/Blazity/elevanceit/main/.github/assets/project-logo.png",
       },
     ],
   },
